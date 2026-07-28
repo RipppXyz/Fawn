2863b2d55d8ad5cccb908f592eb961b7af85d241
RipppXyz <arip30018@gmail.com>
2026-07-28 19:47:22 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "FawnTools",
+  description: "Modern Web Tools",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
